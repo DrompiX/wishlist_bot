5d5c683a94f7c02c6ba66d88087faf15be8218a0
Dima T <mrlocky98@gmail.com>
2020-04-01 19:56:21 +0300

    Messages moved to separate object

---
+++ src/main/scala/tgbot/wishlist/BotMessages.scala
@@ -0,0 +1,9 @@
+package tgbot.wishlist
+
+object BotMessages {
+  val nameRequired = "Please, specify the name of an item after /add command, e.x /add new AirPods."
+  val linkRequired = "Please, specify a link (optional) – type `No` if there is no link."
+  val descRequired = "Please, specify a description (optional) – type `No` if there is no description."
+  val successfulCreation = "New wish was created!"
+  val notFinishedWish = "Previous wish was not finished."
+}
